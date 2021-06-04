a3576f313da8080fc39920abab1c5bafa21db1dd
Johnson <jeremy.m.johnson@citrix-dch.local>
2021-06-04 11:36:03 -0500

    updated MongoClient initialization to use modern syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
+var MongoClient = require('mongodb').MongoClient(url, {useUnifiedTopology: true});
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
