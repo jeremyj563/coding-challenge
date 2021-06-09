be3ebeff46ba3c62ac72b7c8ca08f862f699c4f2
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2021-06-09 11:48:59 -0500

    removed 'Async' suffixes in db/index.js

--- db/index.js
+++ db/index.js
@@ -1,24 +1,24 @@
 import { CONSTANTS, getCollection } from '@local/common';
 import jsonfile from 'jsonfile';
 
-async function getSeedCollectionsAsync() {
+async function getSeedCollections() {
     const customers = await jsonfile.readFile('./customers.json')
     const products = await jsonfile.readFile('./products.json')
     const orders = await jsonfile.readFile('./orders.json')
     return { customers, products, orders }
 }
 
-async function insertCollectionAsync(dbName, collectionName, docs) {
+async function insertCollection(dbName, collectionName, docs) {
     const { collection, client } = await getCollection(dbName, collectionName)
     await collection.insertMany(docs)
     await client.close()
 }
 
 (async function () {
-    const collections = await getSeedCollectionsAsync()
+    const collections = await getSeedCollections()
 
     for (const collectionName in collections) {
         const docs = collections[collectionName]
-        await insertCollectionAsync(CONSTANTS.dbName, collectionName, docs)
+        await insertCollection(CONSTANTS.dbName, collectionName, docs)
     }
 })()
