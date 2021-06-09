58edcc90d7eddef0e655fd1f111d1b4ea525bd14
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2021-06-09 11:47:32 -0500

    initial working db seed implementation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/customers', getCustomers)
+app.get('/products', getProducts)
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
@@ -13,4 +14,9 @@ console.log(`Running on http://${HOST}:${PORT}`)
 async function getCustomers(request, response) {
     const { docs: customers, client } = await getAllDocs('coding-challenge', 'customers')
     response.send(customers)
+}
+
+async function getProducts(request, response) {
+    const { docs: products, client } = await getAllDocs('coding-challenge', 'products')
+    response.send(products)
 }
--- db/index.js
+++ db/index.js
@@ -1,23 +1,51 @@
-import { getCollection } from '@local/common';
+import { CONSTANTS, getCollection } from '@local/common';
+import jsonfile from 'jsonfile';
 
-(async function () {    
-    function newCustomer() {
-        return {
-            firstName: 'John',
-            lastName: 'Doe',
-            address: '123 Hill St',
-            city: 'Melvin',
-            state: 'CA',
-            zip: 93612
-        }
+// function getSeedCollections() {
+//     const customers = jsonfile.readFileSync('./db/customers.json')
+//     const products = jsonfile.readFileSync('./db/products.json')
+//     const orders = jsonfile.readFileSync('./db/orders.json')
+//     return { customers, products, orders
+//     }
+// }
+
+// function insertCollection(dbName, collectionName, docs) {
+// }
+
+// const collections = getSeedCollections()
+
+// for (const collectionName in collections) {
+//     const docs = collections[collectionName]
+//     insertCollection(CONSTANTS.dbName, collectionName, docs)
+// }
+
+async function getSeedCollectionsAsync() {
+    const customers = await jsonfile.readFile('./customers.json')
+    const products = await jsonfile.readFile('./products.json')
+    const orders = await jsonfile.readFile('./orders.json')
+    return { customers, products, orders }
+}
+
+async function insertCollectionAsync(dbName, collectionName, docs) {
+    const { collection, client } = await getCollection(dbName, collectionName)
+    await collection.insertMany(docs)
+    await client.close()
+}
+
+(async function () {
+    const collections = await getSeedCollectionsAsync()
+    //console.log(collections)
+
+    for (const collectionName in collections) {
+        const docs = collections[collectionName]
+        await insertCollectionAsync(CONSTANTS.dbName, collectionName, docs)
     }
-    
-    const { collection, client } = await getCollection('coding-challenge', 'customers')
-    const johnDoe = newCustomer()
-    
-    const cmdResult = await collection.insertOne(johnDoe)
-    console.log(cmdResult.result)
+
+    //collections.forEach(c => await insertCollection(CONSTANTS.dbName, c.key, c.value))
+    //const johnDoe = newCustomer()  
+    //const cmdResult = await collection.insertOne(johnDoe)
+    //console.log(cmdResult.result)
     //const docs = collection.find({})
     //console.log(docs)
-    client.close()
+    //await client.close()
 })()
