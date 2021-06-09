3c8cc23239e1ff930ab0a0b0a783d1e79913c83e
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2021-06-09 11:48:06 -0500

    removed commented code from db/index.js

--- db/index.js
+++ db/index.js
@@ -1,24 +1,6 @@
 import { CONSTANTS, getCollection } from '@local/common';
 import jsonfile from 'jsonfile';
 
-// function getSeedCollections() {
-//     const customers = jsonfile.readFileSync('./db/customers.json')
-//     const products = jsonfile.readFileSync('./db/products.json')
-//     const orders = jsonfile.readFileSync('./db/orders.json')
-//     return { customers, products, orders
-//     }
-// }
-
-// function insertCollection(dbName, collectionName, docs) {
-// }
-
-// const collections = getSeedCollections()
-
-// for (const collectionName in collections) {
-//     const docs = collections[collectionName]
-//     insertCollection(CONSTANTS.dbName, collectionName, docs)
-// }
-
 async function getSeedCollectionsAsync() {
     const customers = await jsonfile.readFile('./customers.json')
     const products = await jsonfile.readFile('./products.json')
@@ -34,18 +16,9 @@ async function insertCollectionAsync(dbName, collectionName, docs) {
 
 (async function () {
     const collections = await getSeedCollectionsAsync()
-    //console.log(collections)
 
     for (const collectionName in collections) {
         const docs = collections[collectionName]
         await insertCollectionAsync(CONSTANTS.dbName, collectionName, docs)
     }
-
-    //collections.forEach(c => await insertCollection(CONSTANTS.dbName, c.key, c.value))
-    //const johnDoe = newCustomer()  
-    //const cmdResult = await collection.insertOne(johnDoe)
-    //console.log(cmdResult.result)
-    //const docs = collection.find({})
-    //console.log(docs)
-    //await client.close()
 })()
