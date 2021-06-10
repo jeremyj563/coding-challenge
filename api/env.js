b19557fff49e1e33f51abeadc188b11592370d6b
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2021-06-09 19:54:26 -0500

    updated api/env.js

--- api/env.js
+++ api/env.js
@@ -1,8 +1,9 @@
 import ip from 'ip'
 import fs from 'fs'
+import os from 'os'
 
 const dbHost = ip.address()
-const dbPort = 27017
+const dbPort = '27017'
 
-fs.writeFileSync('env.list', dbHost)
-fs.appendFileSync('env.list', dbPort)
+fs.writeFileSync('env.list', `DBHOST=${dbHost}`)
+fs.appendFileSync('env.list', `${os.EOL}DBPORT=${dbPort}`)
