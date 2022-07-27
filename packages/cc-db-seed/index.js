51a98cbd9198f8deb47ab6ddf7d61d4c5aed1951
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2022-07-27 17:09:10 -0500

    working docker compose setup

--- packages/cc-db-seed/index.js
+++ packages/cc-db-seed/index.js
@@ -1,10 +1,13 @@
 import { addAllDocs } from '@jeremyj563/cc-common'
 import jsonfile from 'jsonfile'
 
+// import dotenv from 'dotenv'
+// dotenv.config({path: './.env.dev'})
+
 async function getSeedCollections() {
-    const customers = await jsonfile.readFile('./db-seed/customers.json')
-    const products = await jsonfile.readFile('./db-seed/products.json')
-    const orders = await jsonfile.readFile('./db-seed/orders.json')
+    const customers = await jsonfile.readFile('./customers.json')
+    const products = await jsonfile.readFile('./products.json')
+    const orders = await jsonfile.readFile('./orders.json')
     return { customers, products, orders }
 }
 
